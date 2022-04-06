acc6d4953fe87d86c8080d67d6e63e61dc4021c1
mohammedzidan <mohmmedzidane@gmail.com>
2022-04-06 08:57:39 +0300

    update1

--- lib/shoulder.dart
+++ lib/shoulder.dart
@@ -59,33 +59,39 @@ Bginner
                 children: [
                   Padding(
                     padding: const EdgeInsets.only(left: 72.0),
-                    child: Container(
-                      width: 100.0,
-                      height: 100.0,
-                      child: const Center(
-                          child: Text(
-                        "Start",
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 20.0,
-                            fontWeight: FontWeight.w500),
-                      )),
-                      decoration: const BoxDecoration(
-                        color: Color.fromARGB(255, 14, 219, 21),
-                        shape: BoxShape.circle,
+                    child: InkWell(
+                      onTap: (){},
+                      child: Container(
+                        width: 100.0,
+                        height: 100.0,
+                        child: const Center(
+                            child: Text(
+                          "Start",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.w500),
+                        )),
+                        decoration: const BoxDecoration(
+                          color: Color.fromARGB(255, 14, 219, 21),
+                          shape: BoxShape.circle,
+                        ),
                       ),
                     ),
                   ),
                   Padding(
                     padding: const EdgeInsets.only(right: 28.0),
-                    child: Container(
-                        width: 50.0,
-                        height: 50.0,
-                        decoration: BoxDecoration(
-                          color: Colors.grey.shade100,
-                          shape: BoxShape.circle,
-                        ),
-                        child: const Icon(Icons.music_note_sharp)),
+                    child: InkWell(
+                      onTap: (){},
+                      child: Container(
+                          width: 50.0,
+                          height: 50.0,
+                          decoration: BoxDecoration(
+                            color: Colors.grey.shade100,
+                            shape: BoxShape.circle,
+                          ),
+                          child: const Icon(Icons.music_note_sharp)),
+                    ),
                   )
                 ],
               ),
